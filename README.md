35615c069c18acb5a84e251629c89491b48714e1
Polished Studios <fd.github@outlook.com>
2022-05-22 21:14:38 +0200

    Add okteto and kubernetes configuration.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Watchlist Tracker
+
+<p align="center">
+<a href="https://cloud.okteto.com/deploy">
+  <img src="https://okteto.com/develop-okteto.svg" alt="Develop on Okteto">
+</a>
+</p>
--- test-service/src/main/java/com/watchlisttracker/testservice/TestServiceApplication.java
+++ test-service/src/main/java/com/watchlisttracker/testservice/TestServiceApplication.java
@@ -1,4 +1,4 @@
-package com.watchlisttracker.wltuserservice;
+package com.watchlisttracker.testservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
