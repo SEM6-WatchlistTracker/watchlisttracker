a82183bf500cbdff58aebaf24c66568a307f0c89
Polished Studios <fd.github@outlook.com>
2022-05-23 20:38:32 +0200

    Mongodb modifications

--- organization-service/src/main/java/pl/piomin/services/organization/repository/OrganizationRepository.java
+++ organization-service/src/main/java/pl/piomin/services/organization/repository/OrganizationRepository.java
@@ -1,9 +1,16 @@
 package pl.piomin.services.organization.repository;
 
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
 
+// import pl.piomin.services.organization.model.Organization;
+
+// public interface OrganizationRepository extends CrudRepository<Organization, String> {
+	
+// }
+
+import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.piomin.services.organization.model.Organization;
 
-public interface OrganizationRepository extends CrudRepository<Organization, String> {
+public interface OrganizationRepository extends MongoRepository<Organization, String> {
 	
 }
