7f216efcd169878dd16600ab409a8cb6ec203d19
Polished Studios <fd.github@outlook.com>
2022-05-24 15:53:33 +0200

    Fix folder rename

--- test-service/src/main/java/pl/piomin/services/service/controller/OrganizationController.java
+++ test-service/src/main/java/pl/piomin/services/service/controller/OrganizationController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import pl.piomin.services.service.client.DepartmentClient;
 import pl.piomin.services.service.client.EmployeeClient;
 import pl.piomin.services.service.model.Organization;
+import pl.piomin.services.service.model.Department;
 import pl.piomin.services.service.repository.OrganizationRepository;
 
 @RestController
