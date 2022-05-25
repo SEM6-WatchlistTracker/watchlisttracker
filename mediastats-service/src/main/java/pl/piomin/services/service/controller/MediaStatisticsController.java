b80e0bea8b29c48d00b9d29de6f055a111c40b2f
Polished Studios <fd.github@outlook.com>
2022-05-25 15:26:04 +0200

    Fixed mediastats service version

--- mediastats-service/src/main/java/pl/piomin/services/service/controller/MediaStatisticsController.java
+++ mediastats-service/src/main/java/pl/piomin/services/service/controller/MediaStatisticsController.java
@@ -8,8 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
-import org.springframework.retry.annotation.Backoff;
+// import org.springframework.kafka.annotation.RetryableTopic;
+// import org.springframework.retry.annotation.Backoff;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +28,7 @@ public class MediaStatisticsController {
         return ResponseEntity.ok(foundStat);
     }
 
-    @RetryableTopic(attempts = "3", backoff = @Backoff(delay = 2_000, maxDelay = 10_000, multiplier = 2))
+    // @RetryableTopic(attempts = "3", backoff = @Backoff(delay = 2_000, maxDelay = 10_000, multiplier = 2))
     @KafkaListener(groupId = "mediastatistics", topics = "mediastatistics")
     public void updateStatistics(String receivedMessage) {
         LOGGER.info("Received request to update media statistic");
