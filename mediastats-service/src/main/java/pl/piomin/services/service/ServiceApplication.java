3de21185034d25c13991859f5cb0319dd7418ce8
Polished Studios <fd.github@outlook.com>
2022-05-26 21:25:36 +0200

    Added annotation for kafka

--- mediastats-service/src/main/java/pl/piomin/services/service/ServiceApplication.java
+++ mediastats-service/src/main/java/pl/piomin/services/service/ServiceApplication.java
@@ -3,9 +3,11 @@ package pl.piomin.services.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableKafka
 public class ServiceApplication {
 	
 	public static void main(String[] args) {
